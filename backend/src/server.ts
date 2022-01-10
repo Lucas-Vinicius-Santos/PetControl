2e285d07ca6dd631c7f830f9fc9a8292fd2d1155
lucas-vinicius-santos <lucasparaipaba113@gmail.com>
2022-01-10 10:25:29 -0300

    feat: Cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata";
 import express from "express";
 import routes from "./routes";
+import cors from "cors";
 
 import "./database";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(3333, () => console.log("Serve started"));
