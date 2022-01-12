665f8afe19e7724a3e9fccbb41b59861c6bb022c
lucas-vinicius-santos <lucasparaipaba113@gmail.com>
2022-01-12 13:36:06 -0300

    feat: Responsivo ✔

--- frontend/src/app/views/landing/landing.component.ts
+++ frontend/src/app/views/landing/landing.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent {
+  esterEggCounter = 0;
 
-  constructor() { }
+  esterEgg() {
+    if (this.esterEggCounter !== 0) return;
 
-  ngOnInit(): void {
+    this.esterEggCounter++;
+    let urlImg = '../../../assets/cheems/cheems-burger.gif';
+
+    const body = document.querySelector('.container');
+    const esterEggTag = document.createElement('div');
+    const esterEgg = document.createElement('img');
+
+    esterEgg.setAttribute('src', urlImg);
+    esterEggTag.classList.add('cheems');
+    esterEggTag.appendChild(esterEgg);
+    body?.appendChild(esterEggTag);
+
+    setTimeout(() => {
+      body?.removeChild(esterEggTag);
+    }, 3500);
   }
-
 }
